21fe0c7e2f1e1b8b33ed06729da22ac585a55028
jiangydev <jiangydev@qq.com>
2022-03-26 22:02:40 +0800

    fix: modify node file

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,7 +7,7 @@ wget -O /opt/aptos/etc/waypoint.txt https://devnet.aptoslabs.com/waypoint.txt
 wget -O /opt/aptos/etc/node.yaml http://static.aptos.movemove.org/aptos/public_full_node.yaml
 
 wget -O /opt/aptos/etc/seeds.yaml http://static.aptos.movemove.org/aptos/seeds.yaml \
-    && yq ea -i 'select(fileIndex==0).full_node_networks[0].seeds = select(fileIndex==1).seeds | select(fileIndex==0)' node.yaml seeds.yaml
+    && yq ea -i 'select(fileIndex==0).full_node_networks[0].seeds = select(fileIndex==1).seeds | select(fileIndex==0)' /opt/aptos/etc/node.yaml /opt/aptos/etc/seeds.yaml
 
 if [ ! -z "$PRIVATE_KEY" -a ! -z "$PEER_ID" ]; then
   echo "已设置 Private Key 和 Peer ID"
