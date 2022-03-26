70011b69cd48267751a492cfa8899f90a9851c62
jiangydev <jiangydev@qq.com>
2022-03-26 21:31:33 +0800

    feat: modify yaml source

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,9 +4,9 @@ wget -O /opt/aptos/etc/genesis.blob https://devnet.aptoslabs.com/genesis.blob
 
 wget -O /opt/aptos/etc/waypoint.txt https://devnet.aptoslabs.com/waypoint.txt
 
-wget -O /opt/aptos/etc/node.yaml https://raw.githubusercontent.com/aptos-labs/aptos-core/main/docker/compose/public_full_node/public_full_node.yaml
+wget -O /opt/aptos/etc/node.yaml http://static.aptos.movemove.org/aptos/public_full_node.yaml
 
-wget -O /opt/aptos/etc/seeds.yaml https://raw.githubusercontent.com/jiangydev/aptos-node/main/seeds.yaml \
+wget -O /opt/aptos/etc/seeds.yaml http://static.aptos.movemove.org/aptos/seeds.yaml \
     && yq ea -i 'select(fileIndex==0).full_node_networks[0].seeds = select(fileIndex==1).seeds | select(fileIndex==0)' node.yaml seeds.yaml
 
 if [ ! -z "$PRIVATE_KEY" -a ! -z "$PEER_ID" ]; then
